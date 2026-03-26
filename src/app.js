85befa34917f321db952d25b9fdfba291701a062
ankit8840 <ankitdixit.9502@gmail.com>
2026-03-26 10:04:05 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 
 const app = express();
+app.use("/",(re,res,next)=>{
+    //res.send("Ankit")
+    next()
+},(req,res,next)=>{
+    //res.send("second Ankit")
+    next()
+},(req,res)=>{
+    res.send("third resp");
+}
+)
+// app.get("/one/:id/:name",(req,res)=>{
+//     res.send(req.query)
+// })
 
-app.get("/one/:id/:name",(req,res)=>{
-    res.send(req.query)
-})
-
-app.post("/one",(req,res)=>{
-    res.send("my name is test")
-})
+// app.post("/one",(req,res)=>{
+//     res.send("my name is test")
+// })
 
 
-app.use("/one",(req,res)=>{
-    res.send("default route")
-})
+// app.use("/one",(req,res)=>{
+//     res.send("default route")
+// })
 app.listen(2500);
